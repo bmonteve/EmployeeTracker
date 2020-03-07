330acdc2c947736a904a67ab00b817e3ac6589a2
Brian Monteverde <bmonteve@gmail.com>
2020-03-07 11:20:53 -0800

    Finished working condition of the app

--- Develop/app.js
+++ Develop/app.js
@@ -4,13 +4,24 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​const employees = [];
-​const manChoice = [
+
+
+const render = require("./lib/htmlRenderer")
+const employees = [];
+
+const empChoice = [
+    empType = {
+        choices: ["Engineer", "Intern", "Done Adding Employees"],
+        type: "list",
+        message: "Would you like to add an Engineer or Intern?",
+        name: "emp"
+    }
+]
+
+const manChoice = [
     name = {
         type: "input",
         message: "What is the Managers name?",
@@ -32,35 +43,142 @@ const render = require("./lib/htmlRenderer");
         name: "manOffice"
     }
 ]
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​inquirer
-    .prompt(manChoice)
-    .then(response => {
-        const manager = new Manager (
-            response.manName,
-            response.manId,
-            response.manEmail,
-            response.manOffice
-        )
-        employees.push(manager);
-    });
 
-const team = render.render(employees);
+const engChoice = [
+    name = {
+        type: "input",
+        message: "What is the Engineer's name?",
+        name: "engName"
+    },
+    id = {
+        type: "input",
+        message: "What is the Engineer's Id?",
+        name: "engId",
+    },
+    email = {
+        type: "input",
+        message: "What is the Engineer's email?",
+        name: "engEmail"
+    },
+    gitHub = {
+        type: "input",
+        message: "What is the Engineer's GitHub username?",
+        name: "engGit"
+    }
+]
+
+const intChoice = [
+    name = {
+        type: "input",
+        message: "What is the Intern's name?",
+        name: "intName"
+    },
+    id = {
+        type: "input",
+        message: "What is the Intern's Id?",
+        name: "intId",
+    },
+    email = {
+        type: "input",
+        message: "What is the Intern's email?",
+        name: "intEmail"
+    },
+    school = {
+        type: "input",
+        message: "What school did the Intern attend?",
+        name: "intSchool"
+    }
+]
+
+function init() {
+    makeManager();
+}
+
+function makeManager() {
+    inquirer
+        .prompt(manChoice)
+        .then(function (response) {
+            const manager = new Manager(
+                response.manName,
+                response.manId,
+                response.manEmail,
+                response.manOffice
+            )
+            employees.push(manager);
+            makeEmployee();
+        });
+
+}
+
+function makeEmployee() {
+    inquirer
+        .prompt(empChoice)
+        .then(val => {
+            // If the user says yes to another game, play again, otherwise quit the game
+            if (val.emp === "Engineer") {
+                makeEngineer();
+            } else if (val.emp === "Intern") {
+                makeIntern();
+            } else {
+                finishTeam();
+            }
+        });
+}
+
+function makeEngineer() {
+    inquirer
+        .prompt(engChoice)
+        .then(function (response) {
+            const engineer = new Engineer(
+                response.engName,
+                response.engId,
+                response.engEmail,
+                response.engGit
+            )
+            employees.push(engineer);
+            makeEmployee();
+        });
+}
+
+function makeIntern() {
+    inquirer
+        .prompt(intChoice)
+        .then(function (response) {
+            const intern = new Intern(
+                response.intName,
+                response.intId,
+                response.intEmail,
+                response.intSchool
+            )
+            employees.push(intern);
+            makeEmployee();
+        });
+}
+
+function finishTeam() {
+    const team = render(employees);
+    fs.writeFile(outputPath, team, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+
+init();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
